9a22f7d82ec0169a30f8e9397f9ea822f63c0d07
Ali <ali-a-ahmed1097@hotmail.com>
2022-12-23 18:05:54 -0500

    Added some more main tab functionality. Now when a tab is selected, elements are highlighted.

--- src/index.js
+++ src/index.js
@@ -7,4 +7,24 @@ genHome();
 document.body.style.backgroundImage = `url(${img})`;
 
 const homeTab = document.getElementById('home');
-homeTab.addEventListener('click', genHome);
+const menuTab = document.getElementById('menu');
+const contactTab = document.getElementById('contact');
+
+function selectTab(selected, off1, off2) {
+    if (!(selected.classList.contains('selected'))) selected.classList.toggle('selected');
+    if (off1.classList.contains('selected')) off1.classList.toggle('selected');
+    if (off2.classList.contains('selected')) off2.classList.toggle('selected');
+}
+
+homeTab.addEventListener('click', () => {
+    genHome();
+    selectTab(homeTab, menuTab, contactTab);
+});
+
+menuTab.addEventListener('click', () => {
+    selectTab(menuTab, homeTab, contactTab);
+});
+
+contactTab.addEventListener('click', () => {
+    selectTab(contactTab, menuTab, homeTab);
+});
