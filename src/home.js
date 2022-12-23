6ac19aa24af272478baae45979d71e1f5a4358b5
Ali <ali-a-ahmed1097@hotmail.com>
2022-12-23 16:40:40 -0500

    Changed webpack to track images and css. Changed gitignore to only track changes of index.html in dist folder. Added a home.js with related contents to fill the home tab. Changed index.js to dynamically populate index.html.

---
+++ src/home.js
@@ -0,0 +1,9 @@
+export default function genHome(){
+    const hContent = document.getElementById('content');
+    const about = document.createElement('h2');
+    const description = document.createElement('h3');
+    about.textContent = 'About';
+    description.textContent = 'From the deserts of the Gerudo to the peaks of Hebra, Hyrule Canteen is a celebration of the varied ingredients found all across Hyrule. Our chefs have travelled Hyrule from top to bottom to discover the finest ingredients in all of Hyrule and combined them in one delicious plate. Order at Hyrule Canteen today and let your tastebuds celebrate.';
+    hContent.appendChild(about);
+    hContent.appendChild(description);
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import genHome from "./home";
+import img from "./bg-image/hyrule.jpg";
+
+genHome();
+
+document.body.style.backgroundImage = `url(${img})`;
