54abc8db64e8e91b37e85c63da48f8dbed2cc2fc
Ali <ali-a-ahmed1097@hotmail.com>
2022-12-27 15:01:12 -0500

    Menu items now appear on page. All that is left is styling menu items.     Suggestion: use grid.

--- src/menu.js
+++ src/menu.js
@@ -2,12 +2,51 @@ import Entrees from './entree.csv';
 import Desserts from './desserts.csv';
 import Elixirs from './elixirs.csv';
 
+function buildMenu(menu, menuItems) {
+    for (let i = 0; i < menuItems.length; i++){
+        const item = document.createElement('div');
+        const dish = document.createElement('div');
+        const ingredients = document.createElement('div');
+        const price = document.createElement('div');
+
+        dish.textContent = menuItems[i][0];
+        ingredients.textContent = menuItems[i][1];
+        price.textContent = menuItems[i][2];
+
+        item.appendChild(dish);
+        item.appendChild(ingredients);
+        item.appendChild(price);
+
+        menu.appendChild(item);
+    }
+}
+
+function createMenu(title) {
+    const menuDiv = document.createElement('div');
+    menuDiv.classList.add('menus');
+    const mTitle = document.createElement('h2');
+    mTitle.textContent = title;
+    menuDiv.appendChild(mTitle);
+    return menuDiv;
+}
+
 export default function genMenu() {
     const hContent = document.getElementById('content');
     hContent.innerHTML = '';
     hContent.className = '';
+    hContent.style='margin-right: 250px; margin-left: 250px; margin-top: 50px;';
+    hContent.classList.add('menu');
 
-    console.log(Entrees);
-    console.log(Desserts);
-    console.log(Elixirs);
+    const entreeDiv = createMenu('Entrees');
+    buildMenu(entreeDiv, Entrees);
+    
+    const dessertDiv = createMenu('Desserts');
+    buildMenu(dessertDiv, Desserts);
+
+    const elixirDiv = createMenu('Elixirs');
+    buildMenu(elixirDiv, Elixirs)
+
+    hContent.appendChild(entreeDiv);
+    hContent.appendChild(dessertDiv);
+    hContent.appendChild(elixirDiv);
 }
